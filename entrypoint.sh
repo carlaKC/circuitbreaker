2df866e89f34adabc789f3cc650f3d7d7ce3fd05
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-10-21 11:16:06 -0400

    entrypoint: remove progress timestamps from docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-python3 /progress_timestamps.py /raw_data.csv /data.csv
-
 # Append additional args from docker compose / kubernetes
 ARGS="--loadhist=/data.csv $@"
 exec circuitbreaker $ARGS
